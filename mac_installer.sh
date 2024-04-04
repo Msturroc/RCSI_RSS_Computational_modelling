0f44ad6d8d5fc8452700a684d2321ec6c1f3fb39
Marc Sturrock <marcsturrock@protonmail.com>
2024-04-04 13:38:58 +0100

    Add files via upload

--- mac_installer.sh
+++ mac_installer.sh
@@ -15,23 +15,34 @@ juliaup default release
 # Install required Julia packages
 julia -e '
 using Pkg
-Pkg.add(["Plots", "OrdinaryDiffEq", "DataFrames", "CSV", "Agents", "IJulia", "Base64", "CairoMakie", "Distributions", "Dates", "Graphs", "LsqFit", "DrWatson", "GraphMakie", "HTTP", "Random", "LinearAlgebra", "InteractiveDynamics"])
+Pkg.add(["Plots", "OrdinaryDiffEq", "DataFrames", "CSV", "Agents", "IJulia", "Base64", "CairoMakie", "Distributions", "Dates", "Graphs", "LsqFit", "DrWatson", "GraphMakie", "HTTP", "Random", "LinearAlgebra", "InteractiveDynamics"]; io=stderr)
 '
 
-# Set the download URL and destination path
-notebook_url="https://raw.githubusercontent.com/Msturroc/covid_models/main/covid_19_modelling1.ipynb"
-notebook_path="$HOME/Downloads/covid_19_modelling1.ipynb"
 
-# Download the Jupyter Notebook using Julia
-julia -e '
-using HTTP
-notebook_url = "'$notebook_url'"
-notebook_path = "'$notebook_path'"
-response = HTTP.get(notebook_url)
-open(notebook_path, "w") do file
-    write(file, response.body)
-end
-println("Notebook downloaded to $notebook_path")
+# Set the download URLs and destination paths
+notebook_urls=(
+    "https://raw.githubusercontent.com/Msturroc/covid_models/main/covid_19_modelling1.ipynb"
+    "https://raw.githubusercontent.com/Msturroc/covid_models/main/covid_19_modelling2.ipynb"
+)
+notebook_paths=(
+    "$HOME/Downloads/covid_19_modelling1.ipynb"
+    "$HOME/Downloads/covid_19_modelling2.ipynb"
+)
+
+# Download the Jupyter Notebooks using Julia
+for ((i=0; i<${#notebook_urls[@]}; i++))
+do
+    julia -e '
+    using HTTP
+    notebook_url = "'${notebook_urls[$i]}'"
+    notebook_path = "'${notebook_paths[$i]}'"
+    response = HTTP.get(notebook_url)
+    open(notebook_path, "w") do file
+        write(file, response.body)
+    end
+    println("Notebook downloaded to $notebook_path")
+    '
+done
 '
 
 # Launch the Jupyter Notebook
