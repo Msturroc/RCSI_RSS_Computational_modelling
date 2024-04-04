46cb6b18c1c37576c6876258e4a8b90c6918b0ed
Marc Sturrock <marcsturrock@protonmail.com>
2024-04-04 12:40:00 +0100

    Add files via upload

--- mac_installer.sh
+++ mac_installer.sh
@@ -15,31 +15,27 @@ juliaup default stable
 # Install required Julia packages
 julia -e '
 using Pkg
-Pkg.add("Plots")
-Pkg.add("OrdinaryDiffEq")
-Pkg.add("DataFrames")
-Pkg.add("CSV")
-Pkg.add("Agents")
-Pkg.add("IJulia")
-Pkg.add("Base64")
-Pkg.add("CairoMakie")
-Pkg.add("Dates")
-Pkg.add("Distributions")
-Pkg.add("DrWatson")
-Pkg.add("GraphMakie")
-Pkg.add("Graphs")
-Pkg.add("HTTP")
-Pkg.add("InteractiveDynamics")
-Pkg.add("LinearAlgebra")
-Pkg.add("LsqFit")
-Pkg.add("Random")
+Pkg.add(["Plots", "OrdinaryDiffEq", "DataFrames", "CSV", "Agents", "IJulia", "Base64", "CairoMakie", "Distributions", "Dates", "Graphs", "LsqFit", "DrWatson", "GraphMakie", "HTTP", "Random", "LinearAlgebra", "InteractiveDynamics"])
 '
 
-# Set the path to your Jupyter Notebook file
+# Set the download URL and destination path
+notebook_url="https://raw.githubusercontent.com/Msturroc/covid_models/main/covid_19_modelling1.ipynb"
 notebook_path="$HOME/Downloads/covid_19_modelling1.ipynb"
+
+# Download the Jupyter Notebook using Julia
+julia -e '
+using HTTP
+notebook_url = "'$notebook_url'"
+notebook_path = "'$notebook_path'"
+response = HTTP.get(notebook_url)
+open(notebook_path, "w") do file
+    write(file, response.body)
+end
+println("Notebook downloaded to $notebook_path")
+'
 
 # Launch the Jupyter Notebook
 julia -e '
 using IJulia
-notebook(detached=true, dir="'$notebook_path'")
+notebook(detached=true, dir="'$HOME/Downloads'", file="covid_19_modelling1.ipynb")
 '
